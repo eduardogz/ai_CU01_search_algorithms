20d9d9d4aa3dfaa2b71fb091c4e5790c71b521bc
Eduardo Cruz <eduardo.gz@gmail.com>
2017-02-03 07:22:07 -0500

    Created Board class
    The Board class helps manage the n-puzzle board

---
+++ driver.py
@@ -0,0 +1,92 @@
+import math
+
+class Node(object):
+	"""docstring for Node"""
+	def __init__(self, parent, action):
+		super(Node, self).__init__()
+		self.parent = parent
+		self.action = action
+
+class State(object):
+	"""docstring for State"""
+	def __init__(self, arg):
+		super(State, self).__init__()
+		self.arg = arg
+
+class Solver(object):
+	"""docstring for Solver"""
+	def __init__(self, arg):
+		super(Solver, self).__init__()
+		self.arg = arg
+
+class Board(object):
+	"""docstring for Board"""
+	def __init__(self, state: list):
+		super(Board, self).__init__()
+		self.state = state
+		self.dim = int(math.sqrt(len(self.state)))
+		self.emptyAtIndex = 0
+		self.possibleActions = []
+
+		i = 0
+		col = 0
+		row = 0
+		while i < len(self.state): # finds row and col where the zero (empty tile) is at:
+			row = math.floor(i / self.dim)
+			col = i % self.dim
+			if self.state[i] == 0:
+				#print('Found empty slot on: (' + str(row) + ',' + str(col) + ')')
+				self.emptyAtIndex = i
+				break
+			i = i+1
+
+		# determine possible actions based on row and col
+		# visit child nodes in the "UDLR" order
+		if row > 0 :
+			self.possibleActions.append('Up')
+		if row < self.dim - 1 :
+			self.possibleActions.append('Down')
+		if col > 0 :
+			self.possibleActions.append('Left')
+		if col < self.dim - 1 :
+			self.possibleActions.append('Right')
+
+	def doAction(self, action):
+		try:
+			self.possibleActions.index(action)
+		except Exception as e:
+			print('Board.doAction: requested action "'+ action + '" NOT available')
+		#print(self.state)
+		while (True):
+			if action == 'Up':
+				targetCellIndex = self.emptyAtIndex - self.dim
+				targetCellValue = self.state[targetCellIndex]
+				self.state[self.emptyAtIndex] = targetCellValue
+				self.state[targetCellIndex] = 0
+				break
+			if action == 'Down':
+				targetCellIndex = self.emptyAtIndex + self.dim
+				targetCellValue = self.state[targetCellIndex]
+				self.state[self.emptyAtIndex] = targetCellValue
+				self.state[targetCellIndex] = 0
+				break
+			if action == 'Left':
+				targetCellIndex = self.emptyAtIndex - 1
+				targetCellValue = self.state[targetCellIndex]
+				self.state[self.emptyAtIndex] = targetCellValue
+				self.state[targetCellIndex] = 0
+				break
+			if action == 'Right':
+				targetCellIndex = self.emptyAtIndex + 1
+				targetCellValue = self.state[targetCellIndex]
+				self.state[self.emptyAtIndex] = targetCellValue
+				self.state[targetCellIndex] = 0
+				break
+			break # should never get here but protect from infinite loop
+		#print(self.state)
+
+
+testCase1 = Board([3,1,2,0,4,5,6,7,8])
+testCase2 = Board([1,2,5,3,4,0,6,7,8])
+
+testCase1.doAction('Right')
